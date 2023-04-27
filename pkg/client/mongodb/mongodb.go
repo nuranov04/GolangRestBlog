5facc737f439e80c83302ea458c8318485186b9b
Artur <artnyr2004@gmail.com>
2023-04-27 07:51:45 +0600

    create db client

---
+++ pkg/client/mongodb/mongodb.go
@@ -0,0 +1,41 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewClient(ctx context.Context,
+	host, port, username, password, database, authDB string) (*mongo.Database, error) {
+	var mongoDBUrl string
+	var isAuth bool
+	if username == "" && password == "" {
+		mongoDBUrl = fmt.Sprintf("mongodb://%s:%s", host, port)
+	} else {
+		mongoDBUrl = fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+		isAuth = true
+	}
+	clientOptions := options.Client().ApplyURI(mongoDBUrl)
+	if isAuth {
+		if authDB == "" {
+			authDB = database
+		}
+		clientOptions.SetAuth(options.Credential{
+			AuthSource: authDB,
+			Username:   username,
+			Password:   password,
+		})
+	}
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongodb due to error: %v", err)
+	}
+
+	if err = client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("failed to ping to mongodb due to error: %v", err)
+	}
+	return client.Database(database), nil
+}
